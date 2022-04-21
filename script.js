8d3be6414eebe74e4638f82f89ae37c792bea438
Honeylii <oriadeoxy@gmail.com>
2022-04-21 23:23:15 +0100

    Modified

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
-
+const body = document.querySelector("body");
 const timeLog = document.querySelector(".time-log");
+const container = document.querySelector("#clock");
 
 const now = new Date();
 const option = {
@@ -14,7 +15,7 @@ setInterval(() => {
   const option = {
     hour: "numeric",
     minute: "numeric",
-    // second: "numeric",
+    second: "numeric",
   };
   timeLog.textContent = new Intl.DateTimeFormat(
     navigator.language,
@@ -30,16 +31,7 @@ setInterval(() => {
   )},${randomInt(0, 255)},${randomInt(0, 255)})`;
 }, 1000);
 
-let counter = 0;
-
-setInterval(() => {
-  counter++;
-}, 1000);
-
-const container = document.querySelector("#clock");
-
 const p = container.querySelector("p");
-const body = document.querySelector("body");
 
 const onDrag = (e) => {
   //   let getStyle = window.getComputedStyle(container);
@@ -53,5 +45,5 @@ p.addEventListener("mousedown", function () {
   p.addEventListener("mousemove", onDrag);
 });
 p.addEventListener("mouseup", function () {
-  p.removeEventListener("mousemove", onDrag);
+  p.removeEventListener("mouseup", onDrag);
 });
